49ff4816cbcc89e7d7b7773346ba81b916c30866
Janne Marie <janne_m_tvetene@hotmail.com>
2025-02-28 23:14:49 +0100

    animal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,7 @@
 //password validation
 let password = document.getElementById("password");
 let power = document.getElementById("power-point");
+let strengthImage = document.getElementById("strength_img");
 password.oninput = function () {
     let point = 0;
     let value = password.value;
@@ -9,6 +10,15 @@ password.oninput = function () {
         ["1%", "25%", "50%", "75%", "100%"];
     let colorPower = 
         ["#D73F40", "#DC6551", "#F2B84F", "#BDE952", "#3ba62f"];
+
+    // Define image sources for different strength levels
+let imageSources = [
+    "../images/egg1.png" ,   // Red - Very Weak
+    "../images/dragon1.png",        // Orange - Weak
+    "../images/dragon7.png",      // Yellow - Medium
+    "../images/dragon6.png",      // Light Green - Strong
+    "../images/dragon3.png",  // Dark Green - Very Strong
+];
 
     if (value.length >= 6) {
         let arrayTest = 
@@ -21,7 +31,39 @@ password.oninput = function () {
     }
     power.style.width = widthPower[point];
     power.style.backgroundColor = colorPower[point];
+    strengthImage.src = imageSources[point];
+    //strengthImage.style.display = "block"; // Show image when input starts
 };
+/*
+let passwd = document.getElementById("password");
+let strengthImage = document.getElementById("strength_img");
+
+// Define image sources for different strength levels
+let imageSources = [
+    "../public/images/egg1.png" ,   // Red - Very Weak
+    "../images/dragon1.png",        // Orange - Weak
+    "../images/dragon7.png",      // Yellow - Medium
+    "../images/dragon6.png",      // Light Green - Strong
+    "../images/dragon3.png",  // Dark Green - Very Strong
+];
+
+passwd.on_input = function () {
+    let point = 0;
+    let value = passwd.value;
+
+    if (value.length >= 6) {
+        let arrayTest = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+        arrayTest.forEach((item) => {
+            if (item.test(value)) {
+                point += 1;
+            }
+        });
+    }
+
+    // Change image based on password strength
+    strengthImage.src = imageSources[point];
+    strengthImage.style.display = "block"; // Show image when input starts
+};*/
 
 //creates a session - so when refreshing it will not fade in unless you are redirected from intro page
     document.addEventListener("DOMContentLoaded", function () {
